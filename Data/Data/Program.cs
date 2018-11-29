e8f51a3bc92b9d1f9ece5b88f8917ce9d5686ae8
Ricardo Alex da Costa soares <ricardo.soares@iplanrio.rio.rj.gov.br>
2018-11-29 19:43:30 -0200

    Aprimorando Server

--- Data/Data/Program.cs
+++ Data/Data/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace Data
     {
         static void Main(string[] args)
         {
+            Console.Title = "Data";
+            Process.GetCurrentProcess().WaitForExit();
         }
     }
 }
